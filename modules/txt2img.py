c3449c68e558cc99d34e2170cdf5915ab33158d5
listofbanned <listofbanned@gmail.com>
2022-12-26 21:16:06 +0000

    add txt2img fields

--- modules/txt2img.py
+++ modules/txt2img.py
@@ -8,9 +8,16 @@ import modules.processing as processing
 from modules.ui import plaintext_to_html
 
 
-def txt2img(prompt: str, negative_prompt: str, prompt_style: str, prompt_style2: str, steps: int, sampler_index: int, restore_faces: bool, tiling: bool, n_iter: int, batch_size: int, cfg_scale: float, seed: int, subseed: int, subseed_strength: float, seed_resize_from_h: int, seed_resize_from_w: int, seed_enable_extras: bool, height: int, width: int, enable_hr: bool, denoising_strength: float, firstphase_width: int, firstphase_height: int, *args):
+def txt2img(model_name: str, prompt: str, negative_prompt: str, prompt_style: str, prompt_style2: str, steps: int, sampler_index: int, restore_faces: bool, tiling: bool, n_iter: int, batch_size: int, cfg_scale: float, seed: int, subseed: int, subseed_strength: float, seed_resize_from_h: int, seed_resize_from_w: int, seed_enable_extras: bool, height: int, width: int, enable_hr: bool, denoising_strength: float, firstphase_width: int, firstphase_height: int, *args):
+    model = None
+    print(model_name)
+#    for m in shared.sd_models_list:
+#        if m.sd_checkpoint_info.model_name == model_name:
+#            model = m
+#    print(model.sd_checkpoint_info.model_name)
     p = StableDiffusionProcessingTxt2Img(
         sd_model=shared.sd_model,
+        #sd_model=shared.sd_models_list[0],
         outpath_samples=opts.outdir_samples or opts.outdir_txt2img_samples,
         outpath_grids=opts.outdir_grids or opts.outdir_txt2img_grids,
         prompt=prompt,
